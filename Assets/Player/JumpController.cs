a5b49f26d2bbcec2ab6569dc4ef6d9d09a0d2552
Orangjesus <jackledarby591@gmail.com>
2025-09-29 20:02:07 +0100

    Attempt to fix the jump

--- Assets/Player/JumpController.cs
+++ Assets/Player/JumpController.cs
@@ -11,20 +11,21 @@ public class JumpController : MonoBehaviour
     public float jumpPower;
     public Rigidbody2D rb;
 
-    public LayerMask groundLayer;
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.1f;
-    private bool isGrounded;
+
+    
+    
 
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            isGrounded = false;
+           if(Physics2D.Raycast(transform.position, Vector2.down, 0.55f))
+           {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+            }
         }
       
         if (Input.GetKeyUp(KeyCode.Space) && rb.linearVelocity.y > 0)

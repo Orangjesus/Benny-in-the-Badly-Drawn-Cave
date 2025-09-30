fe0b62fa96d4d2891e7d4967aa46c96dd1e7e1fa
Orangjesus <jackledarby591@gmail.com>
2025-09-30 17:09:39 +0100

    General QOL Changes

--- Assets/Player/ItemPickup.cs
+++ Assets/Player/ItemPickup.cs
@@ -2,28 +2,33 @@ using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ItemPickup : MonoBehaviour
 {
 
-    PlayerCombat playerCombat;
+    public PlayerCombat playerCombat;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetComponent<PlayerCombat>().enabled = false;
+        if (SceneManager.GetActiveScene().name == "Room0")
+        {
+            GetComponent<PlayerCombat>().enabled = false;
+        }
     }
 
     // Update is called once per frame
  
         void OnTriggerEnter2D(Collider2D other)
         {
-            bool hasSword = false;
-            other.GetComponent<PlayerCombat>();
+            
+            GetComponent<PlayerCombat>().enabled = false;
             if (other.CompareTag("Player"))
             {
-                hasSword = true;
+                
                 Destroy(gameObject);
-                other.GetComponent<PlayerCombat>();
+                other.GetComponent<PlayerCombat>().enabled = true;
             }
         }
     

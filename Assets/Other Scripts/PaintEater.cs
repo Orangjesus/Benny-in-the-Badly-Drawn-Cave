ac89ffd950be98cc26c7215b538c4aa82104fd07
Orangjesus <jackledarby591@gmail.com>
2025-09-30 14:36:04 +0100

    Organisation In-Engine

--- Assets/Other Scripts/PaintEater.cs
+++ Assets/Other Scripts/PaintEater.cs
@@ -4,27 +4,19 @@ using UnityEngine;
 public class PaintEater : MonoBehaviour
 {
     
-    public GameObject Player;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+        
+        transform.rotation = Quaternion.Euler(0, 0, 30);
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 scale = transform.localScale;
-        transform.localScale = scale;
 
-        if (Player.transform.position.x > transform.position.x)
-        {
-            scale.x = Mathf.Abs(scale.x) * -1;                       
-        }
-        else
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
